9d655a7f26c248d5fb2b952b84bcad21b762e3a6
Mauricio Lima <mauricio.lima@codeminer42.com>
2022-01-18 18:35:40 -0300

    chore: add EctoHelper module

---
+++ test/discuss/ecto_helper_test.exs
@@ -0,0 +1,21 @@
+defmodule Discuss.EctoHelperTest do
+  use ExUnit.Case, async: true
+
+  alias Discuss.EctoHelper
+
+  test "prettify simple errors" do
+    res = EctoHelper.pretty_errors(body: "is required")
+    assert ["Body is required"] = res
+  end
+
+  test "prettify errors with variables" do
+    res =
+      EctoHelper.pretty_errors(
+        login: {"should be at most %{count} character(s)", [count: 10]},
+        message_body: "should not be blank"
+      )
+
+    assert String.equivalent?(Enum.at(res, 0), "Login should be at most 10 character(s)")
+    assert String.equivalent?(Enum.at(res, 1), "Message body should not be blank")
+  end
+end
